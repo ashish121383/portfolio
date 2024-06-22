8a6ad6e4b69ce4d9df7b6b8afa86f35de618b833
ashish121383 <ashish121383@gmail.com>
2024-06-22 07:15:45 -0700

    changes

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree (CSE)",
+    duration: "2013-2016",
+    institution: "Jaypee University Of Engineering and Technology",
+  },
+  {
+    id: 2,
+    title: "Diploma (CSE)",
+    duration: "2010 - 2013",
+    institution: "Jaypee University Of Engineering and Technology",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2009 - 2010",
+    institution: "Saraswati Shishu Mandir Higher Secondary School",
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Ashish Barman",
+  profile: '/ashish.jpg',
+  designation: "Software Developer",
+  description: "My name is Ashish Barman. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on Python,PHP, Wordpress and JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'developerashish121383@gmail.com',
+  phone: '+91-8827230410',
+  address: 'Ward No 04 Samdriyan Tola Unchehra Near Maiher, Satna, Madhyapradesh ',
+  github: 'https://github.com/ashish121383',
+  facebook: 'https://www.facebook.com/ashish.barman.568',
+  linkedIn: 'https://www.linkedin.com/in/ashish-barman-9aa81010a/',   
+  twitter: '#',
+  stackOverflow: '#',
+  leetcode: "#",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
