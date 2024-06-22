057c87091c0bfff7285664270b2577122accaad8
ashish121383 <ashish121383@gmail.com>
2024-06-22 07:30:43 -0700

    changes

---
+++ utils/data/contactsData.js
@@ -0,0 +1,8 @@
+export const contactsData = {
+    email: 'developerashish121383@gmail.com',
+    phone: '+91-8827230410',
+    address: 'Ward No 04 Samdriyan Tola Unchehra Near Maiher, Satna, Madhyapradesh',
+    github: 'https://github.com/ashish121383',
+    facebook: 'https://www.facebook.com/ashish.barman.568',
+    linkedIn: 'https://www.linkedin.com/in/ashish-barman-9aa81010a/',   
+}
---
+++ utils/data/experience.js
@@ -0,0 +1,21 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Sr Software Engineer ',
+    company: "Rws Moravia",
+    duration: "(Aug 2022 - Present)"
+  },
+  {
+    id: 2,
+    title: "Software Engineer",
+    company: "Mobiweb Technology",
+    duration: "(June 2019 - June 2022)"
+  } ,
+  {
+    id: 3,
+    title: "Self Learning",
+    company: "Code and build something in everyday.",
+    duration: "(June 2016 - June 2019)"
+  },
+  
+]
